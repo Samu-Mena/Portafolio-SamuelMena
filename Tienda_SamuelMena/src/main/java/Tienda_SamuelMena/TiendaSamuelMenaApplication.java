4eec6235fa41e6494372a941919e2e2dd52c3ea2
Samuel Mena <samimenagarro@gmail.com>
2026-01-22 18:39:49 -0600

    Semana2
    Creacion de una pagina html

---
+++ Tienda_SamuelMena/src/main/java/Tienda_SamuelMena/TiendaSamuelMenaApplication.java
@@ -0,0 +1,13 @@
+package Tienda_SamuelMena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaSamuelMenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaSamuelMenaApplication.class, args);
+	}
+
+}
---
+++ Tienda_SamuelMena/src/test/java/Tienda_SamuelMena/TiendaSamuelMenaApplicationTests.java
@@ -0,0 +1,13 @@
+package Tienda_SamuelMena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaSamuelMenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

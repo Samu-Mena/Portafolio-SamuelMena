9bdf61fc254d17f0945cae02a7c2e9e2928a9229
Samuel Mena <samimenagarro@gmail.com>
2026-04-08 19:25:19 -0600

    Semana 12

---
+++ Tienda_SamuelMena/src/main/java/Tienda_SamuelMena/repository/VentaRepository.java
@@ -0,0 +1,8 @@
+package Tienda_SamuelMena.repository;
+
+import Tienda_SamuelMena.domain.Venta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VentaRepository extends JpaRepository<Venta, Integer>{
+    
+}

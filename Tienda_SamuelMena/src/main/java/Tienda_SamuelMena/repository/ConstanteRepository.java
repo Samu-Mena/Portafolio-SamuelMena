f9d12f31cc8e4ccc126692b940cd86afe6fa1f1a
Samuel Mena <samimenagarro@gmail.com>
2026-04-08 19:43:05 -0600

    Semana 13

---
+++ Tienda_SamuelMena/src/main/java/Tienda_SamuelMena/repository/ConstanteRepository.java
@@ -0,0 +1,11 @@
+package Tienda_SamuelMena.repository;
+
+import Tienda_SamuelMena.domain.Constante;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConstanteRepository extends JpaRepository<Constante,Integer> {
+    
+    public Optional<Constante> findByAtributo(String atributo);
+    
+}
